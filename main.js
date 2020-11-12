a0d84704951774978cc5e1618fad67658020e433
vegailia <59516901+vegailia@users.noreply.github.com>
2020-11-12 15:24:20 +0330

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+ 
+const prefix = '-';
+ 
+const fs = require('fs');
+ 
+client.commands = new Discord.Collection();
+ 
+const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+for(const file of commandFiles){
+    const command = require(`./commands/${file}`);
+ 
+    client.commands.set(command.name, command);
+}
+ 
+ 
+client.once('ready', () => {
+    console.log('Godofmiss is online!');
+});
+ 
+client.on('message', message =>{
+    if(!message.content.startsWith(prefix) || message.author.bot) return;
+ 
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+ 
+    if(command === 'ping'){
+        client.commands.get('ping').execute(message, args);
+    } else if (command === 'youtube'){
+        client.commands.get('youtube').execute(message, args);
+    } else if (command === 'discord'){
+        client.commands.get('discord').execute(message, args);
+    } else if (command === 'commands'){
+        client.commands.get('commands').execute(message, args);
+    }
+});
+ 
+client.login('NzYzMjYzMjY5NDQzOTkzNjIx.X31KUA.iQIMBVXuHdb33PR8-vgMuc0ULks');
